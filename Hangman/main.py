21abceda77bcdfeaf08bbb04ac239905ef937396
manntalati <37518894+manntalati@users.noreply.github.com>
2022-03-10 09:08:47 -0600

    Update main.py

--- Hangman/main.py
+++ Hangman/main.py
@@ -2,12 +2,15 @@ import random
 from hangman_words import word_list
 from hangman_art import logo
 
+# chose a random word from the hangman word list
+# start number of lives to 6
 end_of_game = False
 chosen_word = random.choice(word_list)
 lives = 6
 
 print(logo)
 
+# display the number of spaces for the words
 display = []
 for i in range(0, len(chosen_word)):
     display.append("_")
@@ -15,10 +18,13 @@ for i in range(0, len(chosen_word)):
 words = []
 i = 0
 while end_of_game == False:
+    # show the letters that the user has already guessed
     if (i != 0):
         print(f"You have already guessed these letters: {', '.join(words)}")
+    # have the user guess a letter
     guess = input("Guess a letter: ").lower()
     j = 0
+    # if the letter is in the word that has been guessed then replace the blank with the letter
     for letter in chosen_word:
         if letter == guess:
             display[j] = guess
@@ -26,6 +32,7 @@ while end_of_game == False:
                 words.append(guess)
         j += 1
 
+    # if the letter is not in the word then the user loses a life and when lives = 0 then end the game
     if guess not in chosen_word:
         lives -= 1
         words.append(guess)
